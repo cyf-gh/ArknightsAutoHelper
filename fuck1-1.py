d80f55fd35d3672b225261139c1978092e29d6bc
cyf-gh <cyf-ms@hotmail.com>
2019-06-11 09:37:24 +0800

    fix run issue

---
+++ fuck1-1.py
@@ -0,0 +1,6 @@
+from Arknights import ArknightsHelper
+from collections import OrderedDict
+
+Ark = ArknightsHelper(504)
+
+Ark.module_battle_slim(c_id='CE-4', set_count=20, set_ai=True)
--- test/test2-1.py
+++ test/test2-1.py
@@ -3,4 +3,4 @@ from collections import OrderedDict
 
 Ark = ArknightsHelper(86)
 
-Ark.module_battle_slim(c_id='2-1', set_count=3, set_ai=True)
+Ark.module_battle_slim(c_id='1-1', set_count=14, set_ai=True)
